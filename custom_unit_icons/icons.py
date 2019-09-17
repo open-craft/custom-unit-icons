09111540630291f2c33909fb5c23d730b55f522c
Agrendalath <piotr@surowiec.it>
2019-09-17 02:52:07 +0200

    [BB-933] Add arg with previous implementation

--- custom_unit_icons/icons.py
+++ custom_unit_icons/icons.py
@@ -1,9 +1,9 @@
-def get_icon(unit):
+def get_icon(prev_fn, unit):
     """
     Get icon specified in modulestore.
     If `default` is specified there, make a fallback to the default implementation.
     """
     icon = getattr(unit, 'icon', 'default')
     if icon == 'default':
-        return getattr(unit, 'get_icon_class', lambda: 'other')()
+        return prev_fn(unit)
     return icon
