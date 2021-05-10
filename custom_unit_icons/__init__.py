8f88f133759ef91352052e5f2c6e1647d5d1c5c0
Agrendalath <piotr@surowiec.it>
2021-05-10 15:36:48 +0200

    [BB-4164] Support legacy icon overrides
    We have been using the integer `icon` field in the modulestore before introducing the `icon_override` one. This adds support for these "old" overrides.

--- custom_unit_icons/__init__.py
+++ custom_unit_icons/__init__.py
@@ -2,6 +2,6 @@
 This plugin allows customizing unit icons in Studio..
 """
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 default_app_config = 'custom_unit_icons.apps.CustomUnitIconsConfig'  # pylint: disable=invalid-name
--- custom_unit_icons/icons.py
+++ custom_unit_icons/icons.py
@@ -1,4 +1,4 @@
-from xblock.fields import String, Scope
+from xblock.fields import Integer, String, Scope
 
 # Make '_' a no-op so we can scrape strings
 # Using lambda instead of `django.utils.translation.ugettext_noop` because Django cannot be imported in this file
@@ -6,7 +6,7 @@ _ = lambda text: text
 
 
 class IconOverrideMixin:
-    """ Allow changing the icon used for this specific instance of this XBlock """
+    """Allow changing the icon used for this specific instance of this XBlock"""
 
     def __init__(self):
         pass
@@ -18,21 +18,53 @@ class IconOverrideMixin:
         scope=Scope.settings,
     )
 
+    # DEPRECATED: We have been using the `icon` field in the modulestore before introducing the `icon_override` one.
+    #  The `icon` field and the `ICON_MAPPING` list are used by the existing courses, which have been created the
+    #  "old" way. These were introduced for keeping the backward compatibility and should be treated as read-only.
+    icon = Integer(
+        display_name=_("Deprecated Icon Override"),
+        default=0,
+        help=_("Deprecated XBlock Icon Override ID"),
+        scope=Scope.settings,
+    )
+
+    ICON_MAPPING = [
+        icon_override.default,
+        'text',
+        'video',
+        'interactive',
+        'downloadable',
+        'ask-question',
+        'assess-question',
+        'text-box',
+    ]
+
+
 def get_icon(prev_fn, unit):
     """
-    Get icon specified in modulestore.
-    If `default` is specified there, make a fallback to the default implementation.
+    Get icon specified in the modulestore.
+
+    If a default value is specified there, make a fallback to a default implementation.
     """
-    icon = getattr(unit, 'icon_override', 'default')
-    if icon == 'default':
+    icon = getattr(unit, 'icon_override', IconOverrideMixin.icon_override.default)
+
+    # DEPRECATED: This check is here for supporting the deprecated `icon` field.
+    if icon == IconOverrideMixin.icon_override.default:
+        icon_id = getattr(unit, 'icon', 0)
+        if icon_id >= len(IconOverrideMixin.ICON_MAPPING):
+            icon_id = 0
+        icon = IconOverrideMixin.ICON_MAPPING[icon_id]
+
+    if icon == IconOverrideMixin.icon_override.default:
         return prev_fn(unit)
     return icon
 
+
 def create_xblock_info(prev_fn, xblock, *args, **kwargs):
     """
-    Get icon specified in modulestore.
-    If `default` is specified there, make a fallback to the default implementation.
+    Create the information needed for client-side XBlockInfo with the original implementation and add an icon
+    field there, so it can be retrieved in the Studio.
     """
     xblock_info = prev_fn(xblock, *args, **kwargs)
-    xblock_info['icon'] = xblock.icon_override
+    xblock_info['icon'] = get_icon(lambda _: IconOverrideMixin.icon_override.default, xblock)
     return xblock_info
